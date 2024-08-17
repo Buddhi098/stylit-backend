d2b9206150e27105c8e63aea3c3f6bde90a76892
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-08-17 19:42:30 +0530

    implement product-service

---
+++ recommendation-service/src/main/java/com.stylit.online/configuration/securityConfig/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.stylit.online.configuration.securityConfig;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class WebSecurityConfig {
+
+    public static final String SHOPPER = "shopper";
+    public static final String SHOP = "shop";
+    public static final String ADMIN = "admin";
+    public static final String COURIER = "courier";
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> {
+                    auth
+                            .requestMatchers("/public/**").permitAll()
+                            .requestMatchers("/shopper/**").hasRole(SHOPPER)
+                            .requestMatchers("/shop/**").hasRole(SHOP)
+                            .requestMatchers("/admin/**").hasRole(ADMIN)
+                            .requestMatchers("/courier/**").hasRole(COURIER)
+                            .anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF if you are using JWT tokens, since they are immune to CSRF attacks
+
+        return http.build();
+    }
+}
