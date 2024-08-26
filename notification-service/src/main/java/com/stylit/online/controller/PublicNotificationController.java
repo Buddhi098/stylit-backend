67574d04278995e8758751af9d0832398d2d79a5
Isuru Piyumal <athukoralashop@gmail.com>
2024-08-26 22:55:09 +0530

    push notifications added

--- notification-service/src/main/java/com/stylit/online/controller/PublicNotificationController.java
+++ notification-service/src/main/java/com/stylit/online/controller/PublicNotificationController.java
@@ -1,12 +1,44 @@
 package com.stylit.online.controller;
 
+import com.stylit.online.dto.NotificationDTO;
+import com.stylit.online.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("public/notification")
-@RequiredArgsConstructor
 @Validated
+@RequiredArgsConstructor
 public class PublicNotificationController {
+
+    @Autowired
+    private final NotificationService notificationService;
+
+    @PostMapping("/create")
+    public ResponseEntity<NotificationDTO> createNotification(@RequestBody NotificationDTO notificationDTO) {
+        NotificationDTO createdNotification = notificationService.createNotification(notificationDTO);
+        return new ResponseEntity<>(createdNotification, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{actorType}/{userId}")
+    public ResponseEntity<List<NotificationDTO>> getNotificationsForActor(
+            @PathVariable String actorType,
+            @PathVariable Long userId) {
+        List<NotificationDTO> notifications = notificationService.getNotificationsForActor(actorType, userId);
+        return new ResponseEntity<>(notifications, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/read")
+    public ResponseEntity<NotificationDTO> updateReadStatus(
+            @PathVariable Long id,
+            @RequestParam boolean read) {
+        NotificationDTO updatedNotification = notificationService.updateReadStatus(id, read);
+        return new ResponseEntity<>(updatedNotification, HttpStatus.OK);
+    }
 }
